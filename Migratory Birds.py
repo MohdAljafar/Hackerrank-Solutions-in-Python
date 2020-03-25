2844077dab9898dd2013b29c6c42d1ca912caea7
MohdAljafar <59632308+MohdAljafar@users.noreply.github.com>
2020-03-24 21:12:48 -0700

    Create Migratory Birds.py

---
+++ Migratory Birds.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr):
+    lst = []
+    for i in range(1, 6):
+        lst.append(arr.count(i))
+    highest = max(lst)
+    return lst.index(highest)+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
