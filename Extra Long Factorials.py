1d701cdaee6029d41b58b6d16fe6c5b298e0b7c3
MohdAljafar <59632308+MohdAljafar@users.noreply.github.com>
2020-04-07 03:12:26 -0700

    Create Extra Long Factorials.py

---
+++ Extra Long Factorials.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    if n == 1:
+        return 1
+    else:
+        return n*extraLongFactorials(n-1)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    result = extraLongFactorials(n)
+    print(result)
