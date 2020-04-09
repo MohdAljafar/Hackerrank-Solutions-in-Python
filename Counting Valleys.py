ac8f0d873f404d0ecbebf28b063bab2334613459
MohdAljafar <59632308+MohdAljafar@users.noreply.github.com>
2020-04-08 21:35:52 -0700

    Create Counting Valleys.py

---
+++ Counting Valleys.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    valleys = 0
+    v = 0    
+    for i in s:
+        if i == 'U':
+            if v == -1:
+                valleys += 1
+            v += 1
+        elif i == 'D':
+            v -= 1
+    return valleys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
